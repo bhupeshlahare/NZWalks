cf1f9609f8890e4fcec6b6747b5f151706dc6cca
bhupeshlahare <bhupeshlahare@gmail.com>
2024-04-14 11:30:08 +0530

    Added domail model to DB

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<NZWalksDBContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
