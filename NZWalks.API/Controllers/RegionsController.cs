3a9878b33159dc8b6424eb42295633c3a12adfcb
bhupeshlahare <bhupeshlahare@gmail.com>
2024-04-14 16:06:42 +0530

    region get and gel all

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class RegionsController : ControllerBase
+	{
+		private readonly NZWalksDBContext _dbContext;
+        public RegionsController(NZWalksDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+		[HttpGet]
+        public IActionResult GetAll()
+		{
+			var regions = _dbContext.regions.ToList();
+			return Ok(regions);
+		}
+
+		[HttpGet]
+		[Route("id")]
+		public IActionResult Get(Guid id)
+		{
+			var region = _dbContext.regions.FirstOrDefault(u => u.Id == id);
+			if (region == null)
+			{
+				return NotFound();
+			}
+			return Ok(region);
+		}
+	}
+}
